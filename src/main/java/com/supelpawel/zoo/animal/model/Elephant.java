6a16b74eb379163eed06bbeabf44eccace92cca4
pablo <pw.supel@gmail.com>
2022-10-11 00:25:20 +0200

    update entities

--- src/main/java/com/supelpawel/zoo/animal/model/Elephant.java
+++ src/main/java/com/supelpawel/zoo/animal/model/Elephant.java
@@ -1,23 +1,13 @@
 package com.supelpawel.zoo.animal.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Entity
 @Data
-@AllArgsConstructor
 public class Elephant extends Animal {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
-  private int foodPortionsPerDay;
-
   public Elephant() {
-    this.foodPortionsPerDay = 20;
+    this.setFoodPortionsPerDay(20);
   }
 }
--- src/main/java/com/supelpawel/zoo/animal/model/Rabbit.java
+++ src/main/java/com/supelpawel/zoo/animal/model/Rabbit.java
@@ -1,23 +1,13 @@
 package com.supelpawel.zoo.animal.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Entity
 @Data
-@AllArgsConstructor
 public class Rabbit extends Animal {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
-  private int foodPortionsPerDay;
-
   public Rabbit() {
-    this.foodPortionsPerDay = 4;
+    this.setFoodPortionsPerDay(4);
   }
 }
